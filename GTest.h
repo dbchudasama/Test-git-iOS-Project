e9095662e6870a7525be43def35a46388bf14759
Divyesh B Chudasama <DivyeshBC@Divyeshs-MacBook-Pro.local>
2014-07-11 19:44:14 +0100

    added a test class

---
+++ GTest.h
@@ -0,0 +1,13 @@
+//
+//  GTest.h
+//  Git Test
+//
+//  Created by Divyesh B Chudasama on 11/07/2014.
+//  Copyright (c) 2014 DivyeshBC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTest : NSObject
+
+@end
---
+++ GTest.m
@@ -0,0 +1,13 @@
+//
+//  GTest.m
+//  Git Test
+//
+//  Created by Divyesh B Chudasama on 11/07/2014.
+//  Copyright (c) 2014 DivyeshBC. All rights reserved.
+//
+
+#import "GTest.h"
+
+@implementation GTest
+
+@end
